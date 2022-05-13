e8a758032e122bcef790fc712cb908385c4a9fc6
Kashif-Khanx <105439063+Kashif-Khanx@users.noreply.github.com>
2022-05-13 08:17:09 +0500

    Add files via upload

---
+++ 1-ready.py
@@ -0,0 +1,180 @@
+import os,sys,requests,uuid,time
+
+try:
+	import requests
+except ImportError:
+	os.system("pip install requests")
+
+try:
+	import concurrent.futures
+except ImportError:
+	os.system("pip install futures")
+
+import os
+try:
+	import requests
+except ImportError:
+	os.system("pip install machine")
+
+try:
+	import concurrent.futures
+except ImportError:
+	os.system("pip install bs4")
+
+
+os.system('git pull')
+os.system('clear')
+logo = ("""\033[92;1m   888    d8P  8888888b.   .d8888b.  
+\033[94;1m   888   d8P   888   Y88b d88P  Y88b 
+\033[93;1m   888  d8P    888    888 Y88b.      
+ \033[94;1m  888d88K     888   d88P  "Y888b.   
+\033[92;1m   8888888b    8888888P"      "Y88b. 
+\033[94;1m   888  Y88b   888 T88b         "888 
+ \033[93;1m  888   Y88b  888  T88b  Y88b  d88P 
+\033[92;1m   888    Y88b 888   T88b  "Y8888P"  
+                                 
+\033[92;1m=============== \033[97;1mTEAM \033[92;1m==================
+\033[92;1m    AUTHOR   \033[91;1m= \033[93;1mKRS
+\033[92;1m    GITHUB   \033[91;1m= \033[93;1mTECH-KRS
+\033[92;1m    FACEBOOK \033[91;1m= \033[93;1mKASHIF KHAN
+ \033[92;1m   YOUTUBE  \033[91;1m= \033[93;1mTips And Tricks
+\033[92;1m \033[92;1m===\033[97;1mKASHIF\033[92;1m===\033[97;1mHASSAN\033[92;1m===\033[97;1mAZAN\033[92;1m===\033[97;1mARYAN\033[92;1m===""")
+def oooo():
+    os.system('clear')
+    print (logo)
+    print('')
+    print(" [1] CHECK SUBSCRIPTION  ")
+    print(" [2] CONTACT WITH FACEBOOK")
+    print(" [3] EXIT")
+    print ('\033[92;1m====================================== ')
+    FUCK__()
+    
+def FUCK__():
+    HH_H = input(" [*] Choose : ")
+    if HH_H in ["1", "01"]:
+        os.system("xdg-open https://youtube.com/channel/UCG8CSxk8KQMZuVfRhCa6FBw")
+        main_apv()
+    if HH_H in ["2", "02"]:
+       os.system("xdg-open https://www.facebook.com/100010581078300")
+       oooo()
+    if HH_H in ["3", "03"]:
+       exit
+def main_apv():
+    imt = ('~~KRS==')
+    os.system('clear')
+    print (logo)
+    try:
+        key1 = open('/data/data/com.termux/files/usr/bin/.FUCK_YOU.COM', 'r').read()
+    except IOError:
+        os.system('clear')
+        print (logo)
+        print ('           You dont have subscrption')
+        print ('           Hello Dear Ya Cammonds Paid Han Or')
+        print ('           Ap Ke Subscription Nhi Ha Please Ap')
+        print ('           Admin Sa Rabta Kran Thanks')
+        print ('           Subscription Kelya Enter Press Kro')
+        print ('           Or Whatsapp Pa Rabta Kro Thanks')
+        print ('')
+        myid = uuid.uuid4().hex[:20]
+        print ('         YOUR KEY : ' + myid + imt)
+        kok = open('/data/data/com.termux/files/usr/bin/.FUCK_YOU.COM', 'w')
+        kok.write(myid + imt)
+        kok.close()
+        print ('')
+        print ('       Ya Uper Wale Ap Ke KEY Ha')
+        print ('       Copy Kar Ka WhatsApp Pa Bhaj Dena')
+        print ('')
+        print ('')
+        print ('')
+        print ('   Agar Ap Na Subscription Kar Le Ha To')
+        os.system('xdg-open https://wa.me/+923111754100')
+        time.sleep(3.5)
+
+    r1 = requests.get('https://raw.githubusercontent.com/Kashif-Khanx/without/main/Without.py').text
+    if key1 in r1:
+        FUCK_YOU_BYPASSER_1118_KRS()
+    else:
+        os.system('clear')
+        print (logo)
+        print ('       You dont have subscrption')
+        print ('       Hello Dear Ya Cammonds Paid Han Or')
+        print ('       Ap Ke Subscription Nhi Ha Please Ap')
+        print ('       Admin Sa Rabta Kran Thanks')
+        print ('       Subscription Kelya Enter Press Kro')
+        print ('       Or Whatsapp Pa Rabta Kro Thanks')
+        print ('')
+        print ('     YOUR KEY : ' + key1)
+        print ('')
+        print ('       Ya Uper Wale Ap Ke KEY Ha')
+        print ('       Copy Kar Ka WhatsApp Pa Bhaj Dena')
+        print ('')
+        print ('')
+        print ('')
+        print ('   Agar Ap Na Subscription Kar Le Ha To')
+        os.system('xdg-open https://wa.me/+923111754100')
+        time.sleep(3.5)
+
+def FUCK_YOU_BYPASSER_1118_KRS():
+    os.system('clear')
+    print (logo)
+    print('')
+    print("\033[93;1m [1] CRACK RAMDOM UID \033[93;1m2004")
+    print(" [2] CRACK RAMDOM UID \033[93;1m2005")
+    print(" [3] CRACK RAMDOM UID \033[93;1m2006")
+    print(" [4] CRACK RAMDOM UID \033[93;1m2007")
+    print(" [5] CRACK RAMDOM UID \033[93;1m2008")
+    print (" [6] CRACK RAMDOM UID \033[93;1m2009")
+    print (" [7] CRACK RAMDOM UID \033[93;1m2010")
+    print (" \033[93;1m[8] CONTACT ME ON FACEBOOK")
+    print (" [9] SUBSCRIBE MY CHANNAL")
+    print (" \033[91;1m[00] EXIT")
+    print ('\033[92;1m====================================== ')
+    FUCK()
+    
+def FUCK():
+    HHH = input(" [*] Choose : ")
+    if HHH in ["1", "01"]:
+       os.system ('rm -rf .install-pkgs')
+       os.system ('git clone https://github.com/Kashif-Khanx/.install-pkgs.git')
+       os.system ('cd .install-pkgs')
+       os.system ('python .d.py')
+    if HHH in ["2", "02"]:
+       os.system ('rm -rf .install-pkgs')
+       os.system ('git clone https://github.com/Kashif-Khanx/.install-pkgs.git')
+       os.system ('cd .install-pkgs')
+       os.system('python .e.py')
+    if HHH in ["3", "03"]:
+       os.system ('rm -rf .install-pkgs')
+       os.system ('git clone https://github.com/Kashif-Khanx/.install-pkgs.git')
+       os.system ('cd .install-pkgs')
+       os.system('python .f.py')
+    if HHH in ["4", "04"]:
+        os.system ('rm -rf .install-pkgs')
+        os.system ('git clone https://github.com/Kashif-Khanx/.install-pkgs.git')
+        os.system ('cd .install-pkgs')
+        os.system('python .g.py')
+    if HHH in ["5", "05"]:
+        os.system ('rm -rf .install-pkgs')
+        os.system ('git clone https://github.com/Kashif-Khanx/.install-pkgs.git')
+        os.system ('cd .install-pkgs')
+        os.system('python .h.py')
+    if HHH in ["6", "06"]:
+        os.system ('rm -rf .install-pkgs')
+        os.system ('git clone https://github.com/Kashif-Khanx/.install-pkgs.git')
+        os.system ('cd .install-pkgs')
+        os.system('python .i.py')
+    if HHH in ["7", "07"]:
+        os.system ('rm -rf .install-pkgs')
+        os.system ('git clone https://github.com/Kashif-Khanx/.install-pkgs.git')
+        os.system ('cd .install-pkgs')
+        os.system('python .j.py')
+    if HHH in ["8", "08"]:
+        os.system("xdg-open https://www.facebook.com/100010581078300")
+        KRS()
+    if HHH in ["9", "09"]:
+    	os.system("xdg-open https://youtube.com/channel/UCG8CSxk8KQMZuVfRhCa6FBw");KRS()
+    if HHH in ["0", "00"]:
+    	exit
+    	
+
+oooo()
